748b9917bbeb47320fe9dc37a4aede4b657ea55e
chai-aur-react <shambharkaranuj@gmail.com>
2026-07-09 20:32:01 +0530

    fix the bug :

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,8 @@ import Hero from "../sections/Hero/Hero";
 import About from "../sections/About/About";
 import Projects from "../sections/Projects/Projects";
 import Timeline from "../sections/Timeline/Timeline";
-import Services from "../sections/Services/services";
-import Contact from "../sections/Contact/contact";
+import Services from "../sections/Services/Services";
+import Contact from "../sections/Contact/Contact";
 
 export default function Home() {
   return (
