9fd93e126179d180c907c1f89ca1d1d27bf2dfa2
chai-aur-react <shambharkaranuj@gmail.com>
2026-07-10 18:21:32 +0530

    rename about to AboutPage.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,21 @@
 import Hero from "../sections/Hero/Hero";
 
-import Services from "../sections/Services/Services";
-import Contact from "../sections/Contact/Contact";
-import About from "../sections/About/About";
+import About from "../sections/About/AboutPage";
 import Projects from "../sections/Projects/Projects";
 import Timeline from "../sections/Timeline/Timeline";
+
+import Services from "../sections/Services/Services";
+import Contact from "../sections/Contact/Contact";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <About/>
-      <Projects/>
-      <Timeline/>
+      <About />
+      <Projects />
+      <Timeline />
       <Services />
-      <Contact/>
+      <Contact />
     </>
   );
 }
